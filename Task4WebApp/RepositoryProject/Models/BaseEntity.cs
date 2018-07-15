caf5c965d37b7cc1001eb299f25c583a29955a9d
TattiS <tanyas81@gmail.com>
2018-07-15 15:30:38 +0300

    Update dbContext

--- Task4WebApp/RepositoryProject/Models/BaseEntity.cs
+++ Task4WebApp/RepositoryProject/Models/BaseEntity.cs
@@ -1,7 +1,10 @@
-﻿namespace DALProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DALProject.Models
 {
 	public class BaseEntity
     {
+		[Key]
 		public int Id { get; set; }
     }
 }
